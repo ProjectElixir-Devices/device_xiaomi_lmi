1591df9d2848b07c62f87526f72e13f4b2d1bc7d
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    lmi: Migrate to Xiaomi power AIDL HAL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- device.mk
+++ device.mk
@@ -22,6 +22,8 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     vendor/qcom/opensource/usb/etc
 
 # Boot animation
@@ -363,7 +365,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.xiaomi-libperfmgr
 
 # QMI
 TARGET_FWK_SUPPORTS_FULL_VALUEADDS := true
