1c417dc49dfc0ead83a9d32668772072fd4219fb
ZenkaBestia <z.mail.6600@gmail.com>
2022-07-06 02:53:46 +0000

    lmi: update blobs from V13.0.3.0.SJKMIXM - import required missing libs from stock   * E sound_trigger_hw: load_audio_hal: ERROR.     dlopen failed: library "libresampler.so" not found:     needed by /vendor/lib/hw/audio.primary.kona.so in namespace (default)    * E vendor.qti.hardware.qteeconnector@1.0-service:      can't load library:libQTEEConnector_listener.so:      dlopen failed: library "libQTEEConnector_listener.so" not found    * E CamX : [ERROR][UTILS] camxosutilslinux.cpp:926 LibMap() dlopen:      dlopen failed: library "libmialgo_video_enhance.so" not found:      needed by /vendor/lib64/camera/components/com.mi.node.AIIE.so in namespace (default)    * E CHIUSECASE: [ERROR] chxutils.cpp:784 LibMap() Failed to load library /vendor/lib64/libarcdualcamsat.so      error dlopen failed: library "/vendor/lib64/libarcdualcamsat.so" not found - Bluetooth Target Upstreams - update HALs entries in manifests - update vendor security patch level
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>
    Signed-off-by: ZenkaBestia <z.mail.6600@gmail.com>

--- device.mk
+++ device.mk
@@ -153,10 +153,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.a2dp@1.0 \
+    android.hardware.bluetooth.a2dp@1.0.vendor \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    vendor.qti.hardware.fm@1.0 \
+    vendor.qti.hardware.fm@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
