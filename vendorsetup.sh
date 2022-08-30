4c9cdda9a447410f58dac6b101977c8f80fb6dec
ZenkaBestia <z.mail.6600@gmail.com>
2022-08-30 09:56:58 +0000

    lmi: add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+#Clone Repository
+git clone -b snow https://github.com/ProjectElixir-Devices/device_xiaomi_lmi device/xiaomi/lmi
+git clone -b snow https://github.com/ProjectElixir-Devices/vendor_xiaomi_lmi vendor/xiaomi/lmi
+git clone -b immensity-next7 --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_lmi kernel/xiaomi/lmi
+
