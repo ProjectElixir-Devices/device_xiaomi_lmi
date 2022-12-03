2d6a11e8d51d3c884baea8e4c139b235196a660b
Chenyang Zhong <zhongcy95@gmail.com>
2022-12-03 14:25:33 +0000

    lmi: power-libperfmgr: rebrand to lmi Lineage is also moving to Pixel's libperfmgr, and they also named it as android.hardware.power-service.xiaomi-libperfmgr which will unfortunately cause a name duplication as we also track lineage's hardware/xiaomi.
    Rename ours to android.hardware.power-service.cepheus-libperfmgr
    so our in-tree HAL can co-exist with the one in hardware/xiaomi.

--- device.mk
+++ device.mk
@@ -365,7 +365,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power-service.lmi-libperfmgr \
     android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
