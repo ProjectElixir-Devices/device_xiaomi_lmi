a849c7951bb365a5b0d2e7fd24e14d9a2ae6666d
ZenkaBestia <z.mail.6600@gmail.com>
2022-12-13 12:26:19 +0000

    lmi: move to Cheetah Desember 2022 fingerprint

--- aosp_lmi.mk
+++ aosp_lmi.mk
@@ -35,7 +35,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := "google/raven/raven:13/TP1A.221005.002/9012097:user/release-keys"
+BUILD_FINGERPRINT := "google/cheetah/cheetah:13/T2B1.221118.006/9375477:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="lmi-user 12 RKQ1.211001.001 V13.0.6.0.SJKCNXM release-keys"
