101af7e33024781152a3e3a51d8202943b2bfb3f
ZenkaBestia <z.mail.6600@gmail.com>
2023-04-16 05:39:28 +0800

    lmi: use AOSP Clang 17.0.0 (r487747)
    Co-authored-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,3 +6,10 @@ git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250
 echo 'Cloning Vendor Tree'
 # Vendor Tree
 git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_lmi.git -b Tiramisu vendor/xiaomi/lmi
+
+echo 'Clone Specific Clang'
+# Clang
+mkdir clang-r487747
+wget -P clang-r487747 https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r487747.tgz
+tar -xvf clang-r487747/clang-r487747.tgz
+mv clang-r487747 prebuilts/clang/host/linux-x86/
