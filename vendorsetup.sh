15c18346e726f32d334acecdbee27239d1ea74da
Zenka <z.mail.6600@gmail.com>
2023-04-23 09:28:56 +0000

    lmi: track prebuilt app repository

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,3 +17,7 @@ mv clang-r487747 prebuilts/clang/host/linux-x86/
 echo 'Clone Kprofiles'
 # Kprofile App
 git clone --depth=1 https://github.com/KProfiles/android_packages_apps_Kprofiles -b main packages/apps/Kprofiles
+
+echo 'Clone Prebuilt Apps Repo'
+# Prebuilt Apps
+git clone --depth=1 https://gitlab.com/ZenkaBestia/device_xiaomi_lmi_prebuilt-apps -b main device/xiaomi/lmi-prebuilt-apps
