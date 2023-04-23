0ede1ad4fffcd275a4732b40c494a10af2855a05
Zenka <z.mail.6600@gmail.com>
2023-04-23 08:56:45 +0000

    lmi: switch to Kitsune kernel * https://github.com/Kyuofox-Studio/android_kernel_xiaomi_lmi branch lineage-20

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 echo 'Cloning Kernel Tree'
 # Kernel Tree
-git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b stable kernel/xiaomi/lmi
+git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_lmi.git -b Kitsune kernel/xiaomi/lmi
 
 
 echo 'Cloning Vendor Tree'
