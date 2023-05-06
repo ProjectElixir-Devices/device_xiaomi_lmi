48f5d51415a0a523714545b378ac27dd646ec40a
ZenkaBestia <z.mail.6600@gmail.com>
2023-05-06 23:34:39 +0800

    Revert "lmi: Setup LLVM kernel flags & Compile with SDClang"
    This reverts commit c6f3711e2d63d4cad24da03fa663a1b1608a1e3f.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,20 +52,8 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 KERNEL_LD := LD=ld.lld
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
-	  DTC_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
-	  AR=llvm-ar \
-	  LD=ld.lld \
-	  LLVM=1 \
-	  LLVM_IAS=1 \
-	  NM=llvm-nm \
-	  OBJCOPY=llvm-objcopy \
-	  OBJDUMP=llvm-objdump \
-	  STRIP=llvm-strip \
-	  HOSTCFLAGS="-fuse-ld=lld \
-	  -Wno-unused-command-line-argument"
-TARGET_KERNEL_ARCH := arm64
+	  DTC_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := sdclang
 TARGET_KERNEL_SOURCE := kernel/xiaomi/lmi
 TARGET_KERNEL_CONFIG := lmi_defconfig
 
