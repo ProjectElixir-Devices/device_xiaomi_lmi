1eefa086a2a939f23b70e3b9ef8322120bfc2d85
ZenkaBestia <z.mail.6600@gmail.com>
2023-05-06 23:30:40 +0800

    Revert "lmi: use AOSP Clang 17.0.0 (r487747)"
    This reverts commit 101af7e33024781152a3e3a51d8202943b2bfb3f.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,13 +7,6 @@ echo 'Cloning Vendor Tree'
 # Vendor Tree
 git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_lmi.git -b Tiramisu vendor/xiaomi/lmi
 
-echo 'Clone Specific Clang'
-# Clang
-mkdir clang-r487747
-wget -P clang-r487747 https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r487747.tgz
-tar -xvf clang-r487747/clang-r487747.tgz
-mv clang-r487747 prebuilts/clang/host/linux-x86/
-
 echo 'Clone Kprofiles'
 # Kprofile App
 git clone --depth=1 https://github.com/KProfiles/android_packages_apps_Kprofiles -b main packages/apps/Kprofiles
