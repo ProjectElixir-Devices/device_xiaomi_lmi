84570c8e2c9af93d75b41395a298224a99e3e809
ZenkaBestia <z.mail.6600@gmail.com>
2023-05-06 23:35:09 +0800

    Revert "lmi: switch to Nexus Kernel"
    This reverts commit 603142fce275cad07da3b2d084e080478faf339a.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,6 @@
 echo 'Cloning Kernel Tree'
 # Kernel Tree
-git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b stable kernel/xiaomi/lmi
-
+git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_lmi.git -b zen_plus-13 kernel/xiaomi/lmi
 
 echo 'Cloning Vendor Tree'
 # Vendor Tree
