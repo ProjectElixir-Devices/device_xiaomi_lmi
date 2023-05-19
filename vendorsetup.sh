c915d07cfa8087074d0e23de1fde66d84ba9b4ec
ZenkaBestia <z.mail.6600@gmail.com>
2023-05-20 00:03:12 +0800

    Revert "lmi: vendorsetup: clone Kprofiles app"
    This reverts commit a330499a9408992c8e6444898df40e3f100d8814.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,10 +6,6 @@ echo 'Cloning Vendor Tree'
 # Vendor Tree
 git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_lmi.git -b Tiramisu vendor/xiaomi/lmi
 
-echo 'Clone Kprofiles'
-# Kprofile App
-git clone --depth=1 https://github.com/KProfiles/android_packages_apps_Kprofiles -b main packages/apps/Kprofiles
-
 echo 'Clone Prebuilt Apps Repo'
 # Prebuilt Apps
 git clone --depth=1 https://gitlab.com/ZenkaBestia/device_xiaomi_lmi_prebuilt-apps -b main device/xiaomi/lmi-prebuilt-apps
