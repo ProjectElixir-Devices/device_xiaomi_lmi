c859ae41ac6e9d114ed2d4aa8a45cf3a0aabbfbe
basamaryan <basam.aryan@gmail.com>
2024-09-06 13:40:49 -0400

    lmi: Shim widevine service with libcrypto-v34.so

--- device.mk
+++ device.mk
@@ -462,6 +462,10 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
+# Shims
+PRODUCT_PACKAGES += \
+    libcrypto-v34
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
