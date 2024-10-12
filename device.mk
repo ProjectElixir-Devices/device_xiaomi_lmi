f2918a6ebd4b465b6cc51bcc2882119477d1a8cd
Kyuofox <kyuofox@gmail.com>
2024-10-12 21:42:42 +0800

    lmi: Switch to AIDL fingerprint service
    Change-Id: I8ac6abd65a91a35d1e6c069ef01dee2184f33ad6
    Signed-off-by: Kyuofox <kyuofox@gmail.com>

--- device.mk
+++ device.mk
@@ -160,8 +160,9 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_lmi \
-    sensors.udfps.lmi
+    android.hardware.biometrics.fingerprint-service.xiaomi_lmi \
+    sensors.udfps.lmi \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
