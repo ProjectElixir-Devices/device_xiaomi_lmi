9b0cbd0d95c456aa87ae9021c25fb4c8ef75cc79
Cosmin Tanislav <demonsingur@gmail.com>
2024-10-20 06:35:06 +0000

    lmi: Fix vendor.qti.imsrtpservice@3.0 ELF checks
    Change-Id: I6f7804052089386a8098a2b0e479fa3ce55b4f7f

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -44,7 +44,8 @@ function lib_to_package_fixup_vendor_variants() {
     fi
     case "$1" in
         vendor.qti.hardware.qccsyshal@1.0 | \
-        vendor.qti.hardware.qccvndhal@1.0)
+        vendor.qti.hardware.qccvndhal@1.0 | \
+        vendor.qti.imsrtpservice@3.0)
            echo "${1}_vendor"
             ;;
         *)
