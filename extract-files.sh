03ca50a32cf645c2e605b25a09cefde825541343
Kyuofox <kyuofox@gmail.com>
2024-11-21 17:57:22 +0800

    lmi: Replace with v33 libcrypto for ELF checks
    Change-Id: I44686b89a0f32229f3dc87784f273d651f0fdd26
    Signed-off-by: Kyuofox <kyuofox@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -78,7 +78,15 @@ function blob_fixup() {
             ;;
         vendor/lib64/libwvhidl.so)
             [ "$2" = "" ] && return 0
-            ${PATCHELF} --replace-needed "libcrypto.so" "libcrypto-v34.so" "${2}"
+            ${PATCHELF} --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
+            ;;
+        vendor/lib/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            ${PATCHELF} --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
+            ;;
+        vendor/lib64/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            ${PATCHELF} --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
             ;;
         vendor/lib/hw/audio.primary.lmi.so)
             [ "$2" = "" ] && return 0
