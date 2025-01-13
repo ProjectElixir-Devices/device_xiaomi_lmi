77f9fb3795a44733af1846f2af566c827088bd38
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    lmi: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- device.mk
+++ device.mk
@@ -326,6 +326,11 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 # Parts
 PRODUCT_PACKAGES += \
     XiaomiParts
