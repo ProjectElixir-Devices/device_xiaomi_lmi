072f29121625a0c3c6aaae110a2be2877b0ca999
pjgowtham <pjgowtham@gmail.com>
2025-01-28 14:02:13 +0800

    lmi: fingerprint: Set dim layer zOrder FAILED: //frameworks/native/services/surfaceflinger:surfaceflinger link surfaceflinger Outputs: out/soong/.intermediates/frameworks/native/services/surfaceflinger/surfaceflinger/android_arm64_armv8-a/unstripped/surfaceflinger Error: exited with code: 1 Command: prebuilts/clang/host/linux-x86/clang-r530567/bin/clang++ out/soong/.intermediates/bionic/libc/crtbegin_dynamic/android_arm64_armv8-a/crtbegin_dynamic.o @out/soong/.intermediates/frameworks/native/services/surfaceflinger/surfacef> Output: ld.lld: error: undefined symbol: _Z17getUdfpsDimZOrderj >>> referenced by OutputLayer.cpp:468 (frameworks/native/services/surfaceflinger/CompositionEngine/src/OutputLayer.cpp:468) >>>               out/soong/.intermediates/frameworks/native/services/surfaceflinger/CompositionEngine/libcompositionengine/android_arm64_armv8-a_static_afdo-surfaceflinger/surfaceflinger.lto.libcompositionengine.a(OutputLayer.o at 14455> clang++: error: linker command failed with exit code 1 (use -v to see invocation)
    Requires: I367b2c395aeb8cb7cb9e3cc38eca98477befe007
    Change-Id: I881db78186fa9c7b350e79ac4ef90f53ee4cfbcf

--- fingerprint/UdfpsExtension.cpp
+++ fingerprint/UdfpsExtension.cpp
@@ -18,6 +18,10 @@
 #include <drm/sde_drm.h>
 #include <compositionengine/UdfpsExtension.h>
 
+uint32_t getUdfpsDimZOrder(uint32_t z) {
+    return z;
+}
+
 uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
     if (touched) {
         z |= FOD_PRESSED_LAYER_ZORDER;
