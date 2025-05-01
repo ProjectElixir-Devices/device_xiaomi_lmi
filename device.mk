b8340c86d7c976d6e0b312d0430184a0a9aa01b9
chrisw444 <wandersonrodriguesf1@gmail.com>
2025-05-02 00:30:02 +0800

    lmi: Build android.hardware.thermal@2.0 vendor variant from source *  CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.limits@1.0-service": library "android.hardware.thermal@2.0.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -481,6 +481,7 @@ PRODUCT_BOOT_JARS += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0 \
+    android.hardware.thermal@2.0.vendor \
     android.hardware.thermal-service.qti
 
 # Touchscreen
