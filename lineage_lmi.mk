0235d20e91c7075102fa64f75c3cd9c8507b03db
S A U R A V <dwarmachine24@gmail.com>
2025-09-24 23:58:12 +0530

    Update lineage_lmi.mk

--- lineage_lmi.mk
+++ lineage_lmi.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from the device configuration.
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
-# Inherit some common Ricedroid stuff.
+# Inherit some common Elixir stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 TARGET_BUILD_APERTURE_CAMERA := false
@@ -23,9 +23,14 @@ PRODUCT_DEVICE := lmi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F2 Pro
-EXTRA_UDFPS_ANIMATIONS := true
-TARGET_DISABLE_EPPE := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Redmi/lmi/lmi:12/SKQ1.211006.001/V14.0.1.0.SJKMIXM:user/release-keys
+
+TARGET_DISABLE_EPPE := true
+
+# Inherit some Project Elixir stuff.
+ELIXIR_BUILD_TYPE := OFFICIAL
+
+EXTRA_UDFPS_ANIMATIONS := true
